0c1c126f471746194957f8bf1ddf8304bd668760
vl <vl@mobitutor.ru>
2020-06-20 20:02:59 +0300

    Part1 functionality finished

--- pkg/card/card.go
+++ pkg/card/card.go
@@ -1,25 +1,47 @@
 package card
 
+import "time"
+
 type Transaction struct {
-	Id int64
-	Amount int64 //сумма операции
+	Id int64 // id-карты
+	Amount int64 //сумма операции в копейках/центах
 	Currency string //валюта операции
-	DateTime uint64 //дата в формате UTC
-	AuthorizationCode string //код авторизации  - уникальный код подтверждения совершения операции
-	MccCode string //код точки продаж (POS-терминал) по каталогу Merchant Category Code
+	DateTime time.Time //дата и время операции в формате UTC
+	Description string //описание операции
 	Status string //текущий статус операции
+	MccCode string //код точки продаж по каталогу Merchant Category Code
+	MccType string //тип точки продаж
 }
 
 type Card struct {
-	Id int64
+	Id int64 // id-транзакции
 	Number string //номер карты
-	ExpiredDate uint64 //срок действия в формате UTC
+	ExpiredDate time.Time //срок действия в формате UTC
 	CvCode string //CV-код
 	PaymentSystem string //платежная система
 	BankIssuer string //банк-эмитент карты
 	CardholderName string //имя и фамилия держателя карты
-	Balance int64 //доступный баланс карты
+	Balance int64 //текущий баланс карты в копейках/центах
 	Currency string //валюта карточного счета
 	Transactions []Transaction
 }
 
+func AddTransaction(card *Card, transaction *Transaction) {
+	card.Transactions = append(card.Transactions, *transaction)
+}
+
+func SumByMCC(transactions []Transaction, mcc []string) int64 {
+	var totalSum int64 = 0
+
+	for _, myTransaction := range transactions { //Перебор элементов транзакций
+
+		for _, myMcc := range mcc { //Перебор элементов набора MCC
+			if myTransaction.MccCode == myMcc {
+				totalSum += myTransaction.Amount
+			}
+		}
+
+	}
+
+	return totalSum
+}
